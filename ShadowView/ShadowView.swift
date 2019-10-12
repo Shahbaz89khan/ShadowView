aa7ef99955da2d2a9bb8ae6b79893ba6d6a147c7
Shahbaz khan <mrcricket33@gmail.com>
2019-10-12 21:06:18 +0400

    ShadowView

---
+++ ShadowView/ShadowView.swift
@@ -0,0 +1,43 @@
+//
+//  ShadowView.swift
+//  ShadowView
+//
+//  Created by Shahbaz Khan on 10/12/19.
+//  Copyright © 2019 Shahbaz Khan. All rights reserved.
+//
+
+import UIKit
+
+
+@IBDesignable open class ShadowView: UIView {
+    
+    private func setupValues(){
+        self.layer.cornerRadius = self.cornerRadius
+        self.layer.shadowColor = self.shadowColor.cgColor
+        self.layer.shadowOffset = self.shadowOffset
+        self.layer.shadowRadius = self.shadowRadius
+        self.layer.shadowOpacity = self.shadowOpacity
+        self.layer.borderWidth = self.borderWidth
+        self.layer.borderColor = self.borderColor.cgColor
+        
+    }
+    
+    open override func prepareForInterfaceBuilder() {
+        self.setupValues()
+        
+    }
+    
+    open override func awakeFromNib() {
+        self.setupValues()
+    }
+    
+    @IBInspectable var cornerRadius: CGFloat = 10
+    @IBInspectable public var borderWidth: CGFloat = 0.0
+    @IBInspectable public var shadowRadius: CGFloat = 5.0
+    @IBInspectable public var shadowOpacity: Float = 0.5
+    @IBInspectable public var borderColor: UIColor = UIColor.init(red: 0, green: 0, blue: 0, alpha: 1.0)
+    @IBInspectable public var shadowColor: UIColor = UIColor.init(red: 0, green: 0, blue: 0, alpha: 1.0)
+    @IBInspectable public var shadowOffset: CGSize = CGSize(width: 0, height: 4)
+    
+}
+
---
+++ ShadowView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShadowView
+//
+//  Created by Shahbaz Khan on 10/12/19.
+//  Copyright © 2019 Shahbaz Khan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
